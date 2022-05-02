7d296940dc052b97a5977177a3b9fe881b167ae9
Claudia Chittim <cchittim10101@gmail.com>
2022-05-02 14:21:51 -0500

    added content to REadMe file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# e-commerce
+# ORM E-Commerce Site
+
+# Purpose of Challenge
+
+This project utlize MySQL, Sequelize a Promised-based Node.js ORM, Visual Studio and Insomnia to teach us solid beck-end user transaction support for an e-commerce database. The purpose of this project was to show us how to see the database using MySQL then make queries using Sequilize to view, alter or delete products from an e-commerce database. With the ORM application we were allowed to create virtual object databases that were used within the coee while managin relational databases. We used Javascript to setup the objects and let Sequelize translate the code into database schema and queries using SQL.
+
+# Resources That Were Utilized For This Challnege:
+
+1. Visual Studio
+2. MySQL2
+3. Dotenv
+4. Javascript
+
+# Link to GitHub Repository
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,57 +3,49 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
-
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }]
+      include: [{ model: Product }],
     });
 
     res.status(200).json(categoryData);
-
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
-
-
-  // create a new category
   try {
-    const categoryData = await Category.findByPk(req.params.id, { include: [{ model: Product }], });
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'Category not found' });
+      res.status(404).json({ message: "Category not found" });
       return;
     }
 
     res.status(200).json(categoryData);
-
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   try {
     const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
-
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
-
   try {
     const updateCategory = await Category.update(
       {
@@ -67,18 +59,17 @@ router.put("/:id", (req, res) => {
     );
 
     if (!updateCategory[0]) {
-      res.status(404).json({ message: 'Category not found' });
+      res.status(404).json({ message: "Category not found" });
       return;
     }
 
-    res.status(200).json('Category Updated');
-
+    res.status(200).json("Category Updated");
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
     const deleteCategory = await Category.destroy({
@@ -88,12 +79,11 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!deleteCategory) {
-      res.status(404).json({ message: 'Category not found' });
+      res.status(404).json({ message: "Category not found" });
       return;
     }
 
-    res.status(200).json('Category Deleted');
-
+    res.status(200).json("Category Deleted");
   } catch (err) {
     res.status(500).json(err);
   }

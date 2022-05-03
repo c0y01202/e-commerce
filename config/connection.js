dd346ae33ad7a83a3f3e236e87b7a43c87197e77
Claudia Chittim <cchittim10101@gmail.com>
2022-05-03 15:36:19 -0500

    changed .env configuration unable to connect to server

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 
+//imports the Sequelize constructor from the library
 const Sequelize = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+//// create connection to our databasee
+const sequelize = new Sequelize("ecommerce_db", "username", "password", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+});
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: "localhod",
+//       dialect: "mysql",
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on 3001!`));
 });

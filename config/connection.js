7f9242e1a3c56424022502152467b05c1256a535
Claudia Chittim <cchittim10101@gmail.com>
2022-05-03 15:53:31 -0500

    checked syntax on connection.js  file

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,10 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.
   {
     host: "localhost",
     dialect: "mysql",
-    port: 3306,
-  }
-);
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+});
 
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)

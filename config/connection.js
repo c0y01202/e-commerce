fd0ef11c1f56532fc190b11e3a353331805b9956
Claudia Chittim <cchittim10101@gmail.com>
2022-05-03 15:50:08 -0500

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 //// create connection to our databasee
-const sequelize = new Sequelize(ecommerce_db,username,process.env.DB_PW)
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW)
   {
     host: "localhost",
     dialect: "mysql",

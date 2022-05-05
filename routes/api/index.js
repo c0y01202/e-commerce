c99e88a9834c67a3ceff4b7efc54d61520139c9a
Claudia Chittim <cchittim10101@gmail.com>
2022-05-05 16:47:10 -0500

    connected to port but express still deprecates

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,3 +9,18 @@ router.use("/products", productRoutes);
 router.use("/tags", tagRoutes);
 
 module.exports = router;
+
+// // GET /api/users
+// router.get('/', (req, res) => {});
+
+// // GET /api/users/1
+// router.get('/:id', (req, res) => {});
+
+// // POST /api/users
+// router.post('/', (req, res) => {});
+
+// // PUT /api/users/1
+// router.put('/:id', (req, res) => {});
+
+// // DELETE /api/users/1
+// router.delete('/:id', (req, res) => {});
